7ac77276ca70fd760b335b1bff84562b24c2b3b3
Will Poynter <will@williampoynter.co.uk>
2025-05-21 12:53:44 +0100

    Integrating Coverage reporting usign Qtly

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
-      provider: "c8",
-      reporter: ["text", "html"],
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
       exclude: ["node_modules/", "dist/", ".yarn/"],
       all: true,
       include: ["src/**/*.{ts,js}"],
